3ddda07e3bd576399dc4ef49c1271bdaf9842442
kolyn092 <kolyn092@gmail.com>
2026-04-13 11:58:12 +0900

    feat: schedule_id로 Comment 리스트 가져오는 함수 추가

--- src/main/java/com/schedule/repository/CommentRepository.java
+++ src/main/java/com/schedule/repository/CommentRepository.java
@@ -3,5 +3,9 @@ package com.schedule.repository;
 import com.schedule.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findAllBySchedule_Id(Long id);
 }
